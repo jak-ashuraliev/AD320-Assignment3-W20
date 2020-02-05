fb89203f13ee466a77a522f829713d284b227526
theKunte <jennykunte90@gmail.com>
2020-02-04 19:33:02 -0800

    addes get json data for colors

--- routes/colors.js
+++ routes/colors.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const colorsJson = require('../data/colors.json');
+
+//Get All Colors
+router.get('/', function (req, res) {
+    res.json(colorsJson);
+});
+
+//Get A Specific Color
+router.get('/:id', function (req, res) {
+    const colors = colorsJson.find(c => c.id === parseInt(req.params.id));    
+    res.send(colors);
+});
 
 module.exports = router;
